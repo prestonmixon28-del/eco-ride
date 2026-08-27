ef1d73c5e1aacfb27b4513af0a92d882ff71d56f
prestonmixon28-del <prestonmixon28@gmail.com>
2026-08-27 05:11:05 -0500

    phase 4 attempted

---
+++ phase4/bin/phase4.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+const double baseRate = 5.0;
+const double serviceFee = 2.0;
+
+String continueChoice = 'y';
+
+while (continueChoice =='y') {
+  try {
+    print('eco-ride rental calculator');
+    print('enter 1 for a bike or 2 for a scooter:');
+
+    String? choiceInput = stdin.readLineSync();
+    int choice = int.parse(choiceInput!)!;
+
+    print('enter your travel distance');
+
+    String? distanceInput = stdin.readLineSync();
+    int distance = int.parse(distanceInput!);
+
+    double cost;
+
+    if (choice == 1) {
+      cost = baseRate * distance;
+      print('you selected a bike');
+    } else if (choice == 2) {
+      cost = baseRate * distance + serviceFee;
+      print('you selected a scooter');
+    } else {
+      print('invalid choice');
+      continue;
+    }
+    print('your total cost is: ${cost.toStringAsFixed(2)}');
+
+    print('do you want to calculate another ride? (y/n)');
+    continueChoice = stdin.readLineSync()!;
+  } on FormatException {
+    print('Invalid input. Please enter a valid number.');
+    print('please try again.');
+  }
+}
+}
