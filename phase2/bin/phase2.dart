47a2cc035e64ae86d3f7f98efb6539dc11db7803
prestonmixon28-del <prestonmixon28@gmail.com>
2026-08-27 04:48:01 -0500

    phase 2 finish

---
+++ phase2/bin/phase2.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  const double baseRate = 5.0;
+  const double serviceFee = 2.0;
+
+  print('eco-ride  rental calculator');
+  print('enter 1 for a bike or 2 for a scooter:');
+
+  String? choiceInput = stdin.readLineSync();
+  int choice = int.tryParse(choiceInput!)!;
+
+  print('enter your travel distance');
+
+  String? distanceInput = stdin.readLineSync();
+  int distance = int.parse(choiceInput!);
+
+  double cost;
+
+  if (choice == 1) {
+    cost = baseRate * distance;
+    print('you selected a bike');
+  } else if (choice == 2) {
+    cost = baseRate * distance + serviceFee;
+    print('you selected a scooter');
+  } else {
+    print('invalid choice');
+    return;
+  }
+  print('your total cost is: ${cost.toStringAsFixed(2)}');
+}
